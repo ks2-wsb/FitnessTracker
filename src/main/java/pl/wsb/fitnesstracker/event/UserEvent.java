1ac568aeda8330b18dee4d8a8bbff88fa182cfe1
ks2-wsb <wrx75868@student.wroclaw.merito.pl>
2026-05-07 16:51:14 +0200

    Nie było mnie na ostatnich zajęciach (Dawid Dudek) i razem z Konradem umówiliśmy się żeby to zrobić razem poza zajęciami.

---
+++ src/main/java/pl/wsb/fitnesstracker/event/UserEvent.java
@@ -0,0 +1,25 @@
+package pl.wsb.fitnesstracker.event;
+
+import jakarta.persistence.*;
+import pl.wsb.fitnesstracker.user.api.User;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "user_event")
+public class UserEvent {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "event_id", nullable = false)
+    private Event event;
+
+    @Column(name = "registration_date", nullable = false)
+    private LocalDate registrationDate;
+}
